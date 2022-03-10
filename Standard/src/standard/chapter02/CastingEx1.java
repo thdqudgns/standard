4e157f4b198f017bb9bac593e9d5dfb55612f8c5
thdqudgns <thdqudgns9612@gmail.com>
2022-03-10 13:06:38 +0900

    형변환 방법

--- Standard/src/standard/chapter02/CastingEx1.java
+++ Standard/src/standard/chapter02/CastingEx1.java
@@ -5,8 +5,9 @@ public class CastingEx1 {
 	
 		//형변환
 		//변환하고자 하는 타입을 괄호와 함께 붙여주기만 하면 된다.
+		
 		double d = 85.4;
-		int score = (int)d;
+		int score = (int)d; //8byte인 double형의 데이터를 4byte인 int형으로 형변환
 		
 		System.out.println("score="+score);
 		System.out.println("d="+d);
