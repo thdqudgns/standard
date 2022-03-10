6a98ccb54abe538adf1bbc1b12ebd3469a27b099
thdqudgns <thdqudgns9612@gmail.com>
2022-03-10 13:07:31 +0900

    문자를 코드로. 인코딩

--- Standard/src/standard/chapter02/CharToCode.java
+++ Standard/src/standard/chapter02/CharToCode.java
@@ -7,11 +7,11 @@ public class CharToCode {
 		char ch = 'A';	// A는 정수로 65이다.
 		int code = (int)ch;	//ch에 저장된 값을 int형으로 변환하여 저장한다.
 		
-		System.out.printf("%c=%d(%#X)%n", ch, code, code);
+		System.out.printf("%c=%d(%#X)%n", ch, code, code); //A=65(0X41)
 		//%c로 ch를 문자로 출력하고, %d로 code를 10진수로 출력하고, %X로 code를 16진수로 출력한다. 
 		
 		char hch = '가';
-		System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+		System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch); //가=44032(0XAC00)
 		//%c로 hch를 문자로 출력하고, %d로 hch를 10진수로 출력하고, %X로 hch를 16진수로 출력한다. 
 	}
 
