7507abd47c6cbb1b52c8ddfa41e859d3358b176b
thdqudgns <thdqudgns9612@gmail.com>
2022-03-10 13:10:33 +0900

    문자열 더하기, 빈 문자열과 결합해도 문자열이 된다, 문자열과 만나면 무엇이든 문자열로 변한다.

--- Standard/src/standard/chapter02/StringEx.java
+++ Standard/src/standard/chapter02/StringEx.java
@@ -9,13 +9,13 @@ public class StringEx {
 		
 		System.out.println(name);
 		System.out.println(str);
-		System.out.println(7 + " "); //숫자 + 공백문자 -> 문자열 '7 '
-		System.out.println(" " + 7); // -> 문자열 ' 7'
-		System.out.println(7 + ""); //숫자 + 빈 문자열 -> 문자열 '7'
-		System.out.println("" + 7); //-> 문자열 '7'
+		System.out.println(7 + " "); //숫자 + 공백문자 -> 문자열인 '7 '
+		System.out.println(" " + 7); // -> 문자열인 ' 7'
+		System.out.println(7 + ""); //숫자 + 빈 문자열 -> 문자열인 '7', 빈 문자열과 결합해도 문자열이 된다.
+		System.out.println("" + 7); //-> 문자열인 '7'
 		System.out.println("" + ""); //빈 문자열 + 빈 문자열 -> ''
 		System.out.println(7 + 7 + ""); //숫자 + 숫자 + 빈문자열 -> 14 : 숫자끼리 합쳐지고, 문자열로 변함
-		System.out.println("" + 7 + 7); //빈문자열 + 숫자 + 숫자 -> 77 : 문자열로 변한 7과 숫자 7이 만나서 문자열 77이 됨
+		System.out.println("" + 7 + 7); //빈문자열 + 숫자 + 숫자 -> 77 : 문자열로 변한 7과 숫자 7이 만나서 문자열인 77이 됨
 		
 		//문자열과 만나면 무엇이든 문자열로 변한다.
 		
