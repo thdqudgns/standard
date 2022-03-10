bd1127e85ae2b9bbfca90f429ddc505774896af6
thdqudgns <thdqudgns9612@gmail.com>
2022-03-10 13:09:34 +0900

    float형의 정밀도는 7자리다

--- Standard/src/standard/chapter02/FloatToBinEx.java
+++ Standard/src/standard/chapter02/FloatToBinEx.java
@@ -7,8 +7,8 @@ public class FloatToBinEx {
 		float f = 9.1234567f;
 		int i = Float.floatToIntBits(f);
 		
-		System.out.printf("%f%n", f);
-		System.out.printf("%X%n", i); //16진수로 출력
+		System.out.printf("%f%n", f); //9.123457, 소수점7번째 자리에서 반올림되어 정수자리 포함 '정밀도 7자리' 지킴
+		System.out.printf("%X%n", i); //4111F9AE, 16진수로 출력
 	}
 
 }
