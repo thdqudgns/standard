f3cca6a18bd6a49ac0de2ee3588590fe17371846
thdqudgns <thdqudgns9612@gmail.com>
2022-03-11 17:27:26 +0900

    소문자를 대문자로 변환하는 프로그램

--- Standard/src/standard/chapter03/OperatorEx15.java
+++ Standard/src/standard/chapter03/OperatorEx15.java
@@ -3,7 +3,12 @@ package standard.chapter03;
 public class OperatorEx15 {
 	public static void main(String args[]) {
 		
+		//소문자를 대문자로 변환하는 프로그램
+		//대문자와 소문자 간의 코드값 차이는 10진수로 32이다
 		
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase - 32);
+		System.out.println(upperCase);	//A
 		
 	} //main method
 
