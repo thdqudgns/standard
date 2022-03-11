0f74193512aed7140e8492778f5e506e8dbd0015
thdqudgns <thdqudgns9612@gmail.com>
2022-03-11 17:24:20 +0900

    부호 연산자 +, -

--- Standard/src/standard/chapter03/OperatorEx04.java
+++ Standard/src/standard/chapter03/OperatorEx04.java
@@ -3,7 +3,18 @@ package standard.chapter03;
 public class OperatorEx04 {
 	public static void main(String args[]) {
 		
+		//부호 연산자 +, -
 		
+		int i = -10;
+		i = +i;
+		System.out.println(i);	//-10
+		
+		i = -10;
+		i = -i;
+		System.out.println(i);	//10
+		
+		//부호 연산자는 덧셈, 뺄셈과 같은 기호를 쓰지만, 다른 연산자다.
+		//피연산자의 개수가 달라서 구별이 가능하다.
 		
 	} //main method
 
