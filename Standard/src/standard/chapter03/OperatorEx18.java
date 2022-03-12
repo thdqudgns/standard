d4616e7aaebfd0f2ec066ddb4e50be301d6d87b4
thdqudgns <thdqudgns9612@gmail.com>
2022-03-12 13:10:45 +0900

    round메서드를 이용한 반올림

--- Standard/src/standard/chapter03/OperatorEx18.java
+++ Standard/src/standard/chapter03/OperatorEx18.java
@@ -3,7 +3,12 @@ package standard.chapter03;
 public class OperatorEx18 {
 	public static void main(String args[]) {
 		
+		//round메서드를 이용한 반올림
+		//round메서드는 매개변수로 받은 값을 소수점 첫째자리에서  반올림을 하고 그 결과를 정수로 돌려준다
 		
+		double pi = 3.141592;
+		double shortPi = Math.round(pi * 1000) / 1000.0;
+		System.out.println(shortPi);	//3.142
 		
 	} //main method
 
