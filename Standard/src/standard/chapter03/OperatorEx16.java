dee4c7b667ac31beff87519a58f011e846922cfa
thdqudgns <thdqudgns9612@gmail.com>
2022-03-12 13:10:19 +0900

    원하는 소숫점까지 나오게 하는 방법

--- Standard/src/standard/chapter03/OperatorEx16.java
+++ Standard/src/standard/chapter03/OperatorEx16.java
@@ -3,7 +3,12 @@ package standard.chapter03;
 public class OperatorEx16 {
 	public static void main(String args[]) {
 		
+		//원하는 소숫점까지 나오게 하는 방법
+		//수를 곱하면서 int형으로 바꿨다가 float형으로 다시 나눈다.
 		
+		float pi = 3.141592f;
+		float shortPi = (int)(pi * 1000) / 1000f;
+		System.out.println(shortPi);	//3.141
 		
 	} //main method
 
