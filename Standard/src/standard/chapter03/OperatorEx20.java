e842935f12a61b1d646537ccb6b8f5a3960771a9
thdqudgns <thdqudgns9612@gmail.com>
2022-03-12 13:11:02 +0900

    % 연산자는 나누는 수(오른쪽)의 부호를 무시한다

--- Standard/src/standard/chapter03/OperatorEx20.java
+++ Standard/src/standard/chapter03/OperatorEx20.java
@@ -3,7 +3,11 @@ package standard.chapter03;
 public class OperatorEx20 {
 	public static void main(String args[]) {
 		
+		//% 연산자는 나누는 수(오른쪽)의 부호를 무시한다
 		
+		System.out.println(-10%8);	//-2
+		System.out.println(10%-8);	//2
+		System.out.println(-10%-8);	//-2
 		
 	} //main method
 
