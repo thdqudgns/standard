2116309c37aba1939252d238caab87aa802fd917
thdqudgns <thdqudgns9612@gmail.com>
2022-03-12 13:10:52 +0900

    나머지 연산자 %

--- Standard/src/standard/chapter03/OperatorEx19.java
+++ Standard/src/standard/chapter03/OperatorEx19.java
@@ -3,7 +3,13 @@ package standard.chapter03;
 public class OperatorEx19 {
 	public static void main(String args[]) {
 		
+		//나머지 연산자 %
 		
+		int x = 10;
+		int y = 8;
+		
+		System.out.printf("%d을 %d로 나누면, %n", x, y);	//10을 8로 나누면, 
+		System.out.printf("몫은 %d이고, 나머지는 %d입니다.%n", x/y, x%y);	//몫은 1이고, 나머지는 2입니다.
 		
 	} //main method
 
