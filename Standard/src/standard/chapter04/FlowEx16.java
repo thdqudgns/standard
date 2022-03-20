ce9cd01e1c7a0256f4583ee6f3483760d03263e5
thdqudgns <thdqudgns9612@gmail.com>
2022-03-20 21:51:44 +0900

    반복문으로 별찍기 5x5

--- Standard/src/standard/chapter04/FlowEx16.java
+++ Standard/src/standard/chapter04/FlowEx16.java
@@ -3,6 +3,22 @@ package standard.chapter04;
 public class FlowEx16 {
 	public static void main(String[] args) {
 		
+		//반복문으로 별찍기 5x5
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		/*
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 * *****
+		 */
 		
 	}//main
 
