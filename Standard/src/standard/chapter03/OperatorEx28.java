7bf0e15e36f9496172cc86fabf629003baba0e1e
thdqudgns <thdqudgns9612@gmail.com>
2022-03-20 20:24:07 +0900

    비트 연산자 |(OR), &(AND), ^(XOR) + Integer.toBinaryString(int i), String.substring(int beginIndex), String.length()

--- Standard/src/standard/chapter03/OperatorEx28.java
+++ Standard/src/standard/chapter03/OperatorEx28.java
@@ -3,7 +3,7 @@ package standard.chapter03;
 public class OperatorEx28 {
 	public static void main(String args[]) {
 		
-		//비트 연산자 |(OR), &(AND), ^(XOR)
+		//비트 연산자 |(OR), &(AND), ^(XOR) + Integer.toBinaryString(int i), String.substring(int beginIndex), String.length()
 		//|(OR)		피연산자 중 한 쪽의 값이 1이면, 1을 결과로 얻는다. 그 외에는 0을 얻는다.
 		//&(AND)	양쪽이 모두 1이어야만 1을 결과로 얻는다. 그 외에는 0을 얻는다.
 		//^(XOR)	피연산자의 값이 서로 다를 때만 1을 결과로 얻는다. 같을 때는 0을 얻는다.
@@ -35,7 +35,7 @@ public class OperatorEx28 {
 		return tmp.substring(tmp.length()-32);
 	}
 	
-	/* Integer.toBinaryString(int i);
+	/* String java.lang.Integer.toBinaryString(int i)
 	 * 
 	 * 10진수 정수형을 2진수의 부호 없는 정수형으로 반환합니다.
 	 * 
@@ -50,7 +50,7 @@ public class OperatorEx28 {
 	 * 		toUnsignedString(int, int)
 	 */
 
-	/* String.substring(int beginIndex);
+	/* String java.lang.String.substring(int beginIndex)
 	 * 
 	 * 이 문자열의 하위 문자열인 문자열을 반환합니다. 
 	 * 서브스트링은 지정된 인덱스의 문자로 시작하여 이 문자열의 끝까지 확장됩니다.
@@ -69,7 +69,7 @@ public class OperatorEx28 {
 	 *  	IndexOutOfBoundsException - if beginIndex is negative or larger than the length of this String object.
 	 */
 
-	/* String.length();
+	/* int java.lang.String.length()
 	 * 
 	 * 이 문자열의 길이를 반환합니다.
 	 * 길이는 문자열 내의 Unicode 코드 단위 수와 동일합니다.
