b5d9b8e85fbe43b422c54f8aecea2cf988653923
thdqudgns <thdqudgns9612@gmail.com>
2022-03-24 18:12:32 +0900

    인스턴스 다루기, 멤버변수의 값 바꾸기

--- Standard/src/standard/chapter06/TvTest02.java
+++ Standard/src/standard/chapter06/TvTest02.java
@@ -2,19 +2,6 @@ package standard.chapter06;
 
 //인스턴스 다루기, 멤버변수의 값 바꾸기
 
-//여기에 적지 않아도, TvTest01에 정의되어 있어서 불러올 수 있다.
-class Tv { 
-	//Tv의 속성(멤버변수)
-	String color;		// 색상
-	boolean power;		// 전원상태 (on/off)
-	int channel;		// 채널
-	
-	//Tv의 기능(메서드)
-	void power() { power = !power; }	// TV를 켜거나 끄는 기능을 하는 메서드
-	void channelUp() { ++channel; }		// TV의 채널을 높이는 기능을 하는 메서드
-	void channelDown() {--channel; }	// TV의 채널을 낮추는 기능을 하는 메서드
-}
-
 class TvTest02 {
 	public static void main(String[] args) {
 		Tv t1 = new Tv();	//참조변수의 선언과 인스턴스의 생성 및 주소대입을 동시에 진행함
@@ -38,3 +25,16 @@ class TvTest02 {
 	*/
 	
 } // class - TvTest02
+
+//여기에 적지 않아도, TvTest01에 정의되어 있어서 불러올 수 있다.
+class Tv { 
+	//Tv의 속성(멤버변수)
+	String color;		// 색상
+	boolean power;		// 전원상태 (on/off)
+	int channel;		// 채널
+	
+	//Tv의 기능(메서드)
+	void power() { power = !power; }	// TV를 켜거나 끄는 기능을 하는 메서드
+	void channelUp() { ++channel; }		// TV의 채널을 높이는 기능을 하는 메서드
+	void channelDown() {--channel; }	// TV의 채널을 낮추는 기능을 하는 메서드
+}
